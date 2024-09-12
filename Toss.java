ec947725246d5cc55823de2f8a3a815642c003c9
Nagashrith Janamanchi <ashrith6974@gmail.com>
2024-09-12 12:01:13 +0530

    ashrith updated

--- Toss.java
+++ Toss.java
@@ -2,5 +2,6 @@ public class Toss {
     public static void main(String[] args) {
         System.out.println("India won the toss");
         System.out.println("Rohit sharma and virat is opening today");
+        System.out.println("strac is opening the bowling");
     }
 }
